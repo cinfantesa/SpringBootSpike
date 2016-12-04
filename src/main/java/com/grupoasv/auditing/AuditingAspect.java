5d61b194d6a6372fdf9880430d43e4d4f6e0d19e
Alfredo Casado <casado.alfredo@gmail.com>
2016-12-04 20:32:21 +0100

    Add feature to audit/log when a user access to a service. Using spring AOP support its posible to implement this feature whiteout touching existing code. The Audit/log functionality is a cross-cutting concern and therefore can be implemented very cleanly through an aspect.

---
+++ src/main/java/com/grupoasv/auditing/AuditingAspect.java
@@ -0,0 +1,16 @@
+package com.grupoasv.auditing;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AuditingAspect {
+
+    @AfterReturning("execution(* com.grupoasv.repository.ServiceRepository+.findOne(..))")
+    public void logServiceAccess(JoinPoint joinPoint) {
+        System.out.println("User Access to service with id: " + joinPoint.getArgs()[0]);
+    }
+}
